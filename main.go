91e761ea56454342469df3ce13dced5e27e10259
Jorge Massih <jorgmassih@gmail.com>
2023-10-04 10:42:30 -0400

    ✨ add swagger integration

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"github.com/gofiber/contrib/swagger"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New();
+	cfg := swagger.Config{
+		BasePath: "/",
+		FilePath: "./docs/swagger.json",
+		Path:     "swagger",
+		Title:    "Swagger API Docs",
+	}
+
+	app.Use(swagger.New(cfg))
 
 	app.Get("/ping", func(c *fiber.Ctx) (err error) {
 		err = c.SendString("pong");
